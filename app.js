71c29b565ab53dc1c35e22877242983707c652e0
unknown <clementsiaffu95@gmail.com>
2020-09-15 16:24:33 +0300

    scond commmit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ require('./models/db.js')
 var routes = require('./routes/index')
 
 app.get('/give/:level',routes.level)
-app.get('/alcohol/pata',routes.getLevel)
+app.get('/pata',routes.getLevel)
 app.listen(PORT,()=>{
     console.log(`listening to the PORT:${PORT}`)
 })
